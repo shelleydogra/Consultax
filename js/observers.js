7937bd751751b74429fa149322986387e503f815
Shelley Dogra <sdogra@Shelleys-MacBook-Pro.local>
2020-01-21 23:35:06 -0600

    appear on scroll faders in

--- js/observers.js
+++ js/observers.js
@@ -1,6 +1,8 @@
 const header = document.querySelector("nav");
 const sectionOne = document.querySelector(".home-hero");
 
+const faders = document.querySelectorAll('.fade-in');
+
 const sectionOneOptions = {
     rootMargin: "0px 0px 0px 0px"
 };
@@ -13,13 +15,33 @@ const sectionOneObserver = new IntersectionObserver(function(
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 header.classList.add("reverse-navbar");
-                console.log(entry);
+                
             } else {
                 header.classList.remove("reverse-navbar");
-                console.log(entry);
+                
             }
         });
     },
     sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            entry.target.classList.remove('appear');
+        } else {
+            entry.target.classList.add('appear');
+            //appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
